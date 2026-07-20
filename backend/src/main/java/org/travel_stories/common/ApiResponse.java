89a2cc99524026e4842c792d69966ade0b9eba14
JayeshBelose <jayeshbelose2006@gmail.com>
2026-07-20 19:42:45 +0530

    Added generic ApiResponse wrapper

---
+++ backend/src/main/java/org/travel_stories/common/ApiResponse.java
@@ -0,0 +1,19 @@
+package org.travel_stories.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+}
